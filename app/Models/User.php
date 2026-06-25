77d627b79aef33b2bcb93fbf2e1cfc2cffc88998
fikrudzulfahmi <fikrudzulfahmi@gmail.com>
2026-06-25 11:10:26 +0700

    Initial commit: Setup Laravel 12 API with UUID, Sanctum, Spatie, and Activity Log

---
+++ app/Models/User.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Laravel\Sanctum\HasApiTokens; // 1. Import Sanctum
+use Spatie\Permission\Traits\HasRoles; // 2. Import Spatie Permission
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+    use HasUuids;
+    use HasApiTokens; // 3. Gunakan trait Sanctum
+    use HasRoles; // 4. Gunakan trait Spatie
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
